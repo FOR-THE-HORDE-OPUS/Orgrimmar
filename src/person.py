6a54f3518daaa53f466cf9623b0021556dced59e
sli <sli@opusonesolutions.com>
2020-01-28 15:34:53 -0500

    Updated readme to reflect planning.

---
+++ src/person.py
@@ -0,0 +1,6 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+    def update_name(self, name):
+        self.name = name
---
+++ tests/test_person.py
@@ -0,0 +1,6 @@
+def test_update_person_name():
+    return
+
+
+def test_update_person_age():
+    return
