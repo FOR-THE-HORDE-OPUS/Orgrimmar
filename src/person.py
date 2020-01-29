85e91379803afd9c54a1c4e3d42a375dc2413ea7
sli <sli@opusonesolutions.com>
2020-01-29 11:00:06 -0500

    Test out some commit messages

--- src/person.py
+++ src/person.py
@@ -2,7 +2,7 @@ class Person:
     def __init__(self, name):
         self.name = name
 
-    def update_name(self, name):
+    def set_name(self, name):
         self.name = name
 
     def set_gender(self, gender):
